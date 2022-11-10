0a31938d5de7050d51cff27013da9c44e0257af2
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-10 16:21:12 +0900

    cleaning up code for sanity

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import styled from "styled-components";
 import Main from "../components/Main";
 import Sidebar from "../components/Sidebar";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ethers } from "ethers";
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.NEXT_PUBLIC_METAMASK_KEY,
+    ethers.getDefaultProvider("https://goerli.infura.io/v3/") // wrong rpc address
+  )
+);
 
 const Dashboard = ({ address }) => {
+  const [sanityTokens, setSanityTokens] = useState([]);
+  const [thirdWebTokens, setThirdWebTokens] = useState([]);
+  useEffect(() => {
+    const getSanityAndthirdWebTokens = async () => {
+      const coins = await fetch(
+        "https://v2wdb4j1.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'coins'%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
+      );
+      const sanityTokens = (await coins.json()).result;
+      setSanityTokens(sanityTokens);
+
+      setThirdWebTokens(
+        sanityTokens.map((token) => sdk.getTokenModule(token.contractAddress))
+      );
+    };
+    return () => {
+      getSanityAndthirdWebTokens();
+    };
+  }, []);
+  console.log("Sanity", sanityTokens);
+  console.log("Thirdweb", thirdWebTokens);
   return (
     <Wrapper>
       <Sidebar />
