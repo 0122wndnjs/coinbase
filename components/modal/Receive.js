e43f6c594a67998a139ca7fcdfe294b69e27b299
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-16 11:46:17 +0900

    adding Receive.js file

---
+++ components/modal/Receive.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
+import { client } from "../../lib/sanity";
+import { BiCopy } from "react-icons/bi";
+import { FaCheck } from "react-icons/fa";
+
+const Receive = () => {
+  return <div></div>;
+};
+
+export default Receive;
